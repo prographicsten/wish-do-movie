31abf9be479f82b337876885f7f13f0a6a0fccb4
prographicsten <prographicsten@gmail.com>
2023-09-02 15:50:00 +0600

    new code added

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,3 @@
 document.getElementById('wish_tube').addEventListener('click', function () {
     window.location.href = 'index.html';
 });
-
-document.getElementById('blog_content').addEventListener('click', function () {
-    window.location.href = 'blog.html';
-});
